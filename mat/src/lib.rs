04b62d415cd85481938fd6774bf9cafc45ba4a9a
Fillipe143 <fillipedev.contato@gmail.com>
2024-06-26 16:39:23 -0300

    fixed rows and cols exchanged in the mat macro

--- mat/src/lib.rs
+++ mat/src/lib.rs
@@ -17,17 +17,17 @@ macro_rules! mat {
             };
 
             $(
-                let mut rows = 0;
-                tmp_mat.cols += 1;
+                let mut cols = 0;
+                tmp_mat.rows += 1;
                 $(
-                    rows += 1;
+                    cols += 1;
                     tmp_mat.data.push($value);
                 )+
 
-                if tmp_mat.rows == 0 {
-                    tmp_mat.rows = rows;
+                if tmp_mat.cols == 0 {
+                    tmp_mat.cols = cols;
                 } else {
-                    assert!(tmp_mat.rows == rows, "Number of element in each row must be equal.");
+                    assert!(tmp_mat.cols == cols, "Number of element in each row must be equal.");
                 }
             )+
 
@@ -97,8 +97,8 @@ mod tests {
             4, 5;
         ];
 
-        assert_eq!(m.rows, 2, "Invalid number of rows. Expected {} but got {}.", 2, m.rows);
-        assert_eq!(m.cols, 3, "Invalid number of cols. Expected {} but got {}.", 3, m.cols);
+        assert_eq!(m.rows, 3, "Invalid number of rows. Expected {} but got {}.", 2, m.rows);
+        assert_eq!(m.cols, 2, "Invalid number of cols. Expected {} but got {}.", 3, m.cols);
         assert_eq!(m.data.len(), m.rows * m.cols, "Invalid number of elements. Expected {} but got {}.", m.rows*m.cols, m.data.len());
         for i in 0..5 { assert_eq!(m.data[i], i, "Invalid element in position. Expected {} but got {}.", i, m.data[i]); }
     }
